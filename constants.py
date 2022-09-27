b8d3077049e8ac62732b816af3c66f7b30b82931
Tare Ebelo <75279482+TareHimself@users.noreply.github.com>
2022-09-27 12:01:51 -0400

    plan to integrate rasa

--- constants.py
+++ constants.py
@@ -11,7 +11,7 @@ vosk_model_url = "https://alphacephei.com/vosk/models/vosk-model-en-us-daanzu-20
 wx_color_darkgrey = wx.Colour(31, 31, 31)
 wx_visualizer_band_color = wx.Colour(255, 255, 255)
 main_window_name = "Voice"
-wake_word ="smart"
+wake_word ="alice"
 secrets = {}
 with open(SECRETS_PATH, "r") as infile:
     secrets = json.load(infile)
--- skills/__init__.py
+++ skills/__init__.py
@@ -1,7 +1,23 @@
 import re
+from rasa.cli.utils import get_validated_path
+from rasa.model import get_model, get_model_subdirectories
+from rasa.core.interpreter import RasaNLUInterpreter
+from rasa.shared.nlu.training_data.message import Message
 
-all_skills = {}
+def load_interpreter(model_dir, model):
+    path_str = str(pathlib.Path(model_dir) / model)
+    model = get_validated_path(path_str, "model")
+    model_path = get_model(model)
+    _, nlu_model = get_model_subdirectories(model_path)
+    return RasaNLUInterpreter(nlu_model)
 
+# Loads the model
+mod = load_interpreter(model_dir, model)
+# Parses new text
+msg = Message({TEXT: text})
+for p in interpreter.interpreter.pipeline:
+    p.process(msg)
+    print(msg.as_dict())
 
 def Skill(regex):
     def inner(func):
@@ -23,6 +39,7 @@ def RegisterSkill(s, regex):
 
 
 def GetCommand(phrase):
+    interpreter.parse(phrase)
     for key in all_skills:
         if re.match(key, phrase, re.IGNORECASE):
             groups = list(re.search(key, phrase, re.IGNORECASE).groups())
